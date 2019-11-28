57e2b950de04d7ccb9a0a3e9bb7cb294ccb7992b
Angelito Salud <asalud@you-source.com>
2019-11-28 18:39:38 +0800

    Fixed bug with greater than less than comparison

--- src/core/base-step.ts
+++ src/core/base-step.ts
@@ -28,8 +28,8 @@ export abstract class BaseStep {
     notbe: 'Expected %s field not to be %s, but it was also %s',
     contain: 'Expected %s field to contain %s, but it is not contained in %s',
     notcontain: 'Expected %s field not to contain %s, but it is contained in %s',
-    begreaterthan: '%s field with value %s was not greater than %s',
-    belessthan: '%s field with value %s was not less than %s',
+    begreaterthan: '%s field is expected to be greater than %s, but its value was %s',
+    belessthan: '%s field is expected to be less than %s, but its value was %s',
   };
 
   operatorSuccessMessages = {
@@ -85,20 +85,20 @@ export abstract class BaseStep {
       } else if (operator == 'not contain') {
         return !actualValue.includes(value);
       } else if (operator == 'be greater than') {
-        if (dateTimeFormat.test(value) && dateTimeFormat.test(actualValue)) {
+        if (dateTimeFormat.test(actualValue) && dateTimeFormat.test(value)) {
           return moment(actualValue).isAfter(value);
-        } else if (!isNaN(Number(value)) && !isNaN(Number(actualValue))) {
-          return parseFloat(value) > parseFloat(actualValue);
+        } else if (!isNaN(Number(actualValue)) && !isNaN(Number(value))) {
+          return parseFloat(actualValue) > parseFloat(value);
         } else {
-          throw new Error(`Couldn't check that ${value} > ${actualValue}. The ${operator} operator can only be used with numeric or date values.`);
+          throw new Error(`Couldn't check that ${actualValue} > ${value}. The ${operator} operator can only be used with numeric or date values.`);
         }
       } else if (operator == 'be less than') {
-        if (dateTimeFormat.test(value) && dateTimeFormat.test(actualValue)) {
+        if (dateTimeFormat.test(actualValue) && dateTimeFormat.test(value)) {
           return moment(actualValue).isBefore(value);
-        } else if (!isNaN(Number(value)) && !isNaN(Number(actualValue))) {
-          return parseFloat(value) < parseFloat(actualValue);
+        } else if (!isNaN(Number(actualValue)) && !isNaN(Number(value))) {
+          return parseFloat(actualValue) < parseFloat(value);
         } else {
-          throw new Error(`Couldn't check that ${value} > ${actualValue}. The ${operator} operator can only be used with numeric or date values.`);
+          throw new Error(`Couldn't check that ${actualValue} < ${value}. The ${operator} operator can only be used with numeric or date values.`);
         }
       }
     } else {
