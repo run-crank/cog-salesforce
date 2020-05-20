ce438e40cff74816582006aacb12c27252bb5261
Eric Peterson <i.am@eric.pe>
2020-05-20 14:02:44 -0700

    Refactor OpportunityAware mixin to use ObjectAwareMixin as its base.

--- src/client/mixins/opportunity-aware.ts
+++ src/client/mixins/opportunity-aware.ts
@@ -1,6 +1,7 @@
 import * as jsforce from 'jsforce';
+import { ObjectAwareMixin } from './object-aware';
 
-export class OpportunityAwareMixin {
+export class OpportunityAwareMixin extends ObjectAwareMixin {
   clientReady: Promise<boolean>;
   client: jsforce.Connection;
 
@@ -10,21 +11,7 @@ export class OpportunityAwareMixin {
    * @param {Record<string, any>} opportunity - The Oppurtunity record to create.
    */
   public async createOpportunity(opportunity: Record<string, any>): Promise<jsforce.SuccessResult> {
-    await this.clientReady;
-    return new Promise((resolve, reject) => {
-      try {
-        this.client.sobject('Opportunity').create(opportunity, (err, result: any) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve(result);
-        });
-      } catch (e) {
-        reject(e);
-      }
-    });
+    return this.createObject('Opportunity', opportunity);
   }
 
   /**
@@ -34,20 +21,8 @@ export class OpportunityAwareMixin {
    * @param {String} identifier - the value of the id field to use when searching.
    */
   public async findOpportunityByIdentifier(idField: string, identifier: string): Promise<Record<string, any>[]> {
-    await this.clientReady;
-    return new Promise((resolve, reject) => {
-      try {
-        this.client.sobject('Opportunity').find({ [idField]: identifier }, (err, records) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve(records);
-        });
-      } catch (e) {
-        reject(e);
-      }
+    return this.findObjectsbyFields('Opportunity', {
+      [idField]: identifier,
     });
   }
 
@@ -58,28 +33,20 @@ export class OpportunityAwareMixin {
    * @param {String} identifier - the value of the id field to use when searching.
    */
   public async deleteOpportunityByIdentifier(idField: string, identifier: string): Promise<Record<string, any>> {
-    await this.clientReady;
     return new Promise(async (resolve, reject) => {
       try {
-        const opportunity = await this.findOpportunityByIdentifier(idField, identifier);
-        if (opportunity.length > 1) {
+        const opps = await this.findOpportunityByIdentifier(idField, identifier);
+        if (opps.length > 1) {
           reject(new Error(`More than one opportunity matches ${idField} ${identifier}`));
           return;
         }
 
-        if (!opportunity || !opportunity[0].Id) {
+        if (!opps || !opps[0].Id) {
           reject(new Error(`No Account found with ${idField} ${identifier}`));
           return;
         }
 
-        this.client.sobject('Opportunity').delete(opportunity[0].Id, (err, result: any) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          resolve(opportunity[0]);
-        });
+        resolve(await this.deleteObjectById('Opportunity', opps[0].Id));
       } catch (e) {
         reject(e);
       }
