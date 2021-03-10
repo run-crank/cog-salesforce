ed32a224d1e2d53ef04fa2d7e043420f6af1610a
Angelito Salud <asalud@you-source.com>
2021-03-10 17:16:20 +0800

    Add Task Field Check Step

---
+++ src/steps/task/task-field-equals.ts
@@ -0,0 +1,132 @@
+import { Field, ExpectedRecord } from './../../core/base-step';
+/*tslint:disable:no-else-after-return*/
+
+import { BaseStep, StepInterface } from '../../core/base-step';
+import { Step, RunStepResponse, FieldDefinition, StepDefinition, RecordDefinition } from '../../proto/cog_pb';
+import * as util from '@run-crank/utilities';
+import { baseOperators } from '../../client/constants/operators';
+import { isNullOrUndefined } from 'util';
+
+export class TaskFieldEquals extends BaseStep implements StepInterface {
+
+  protected stepName: string = 'Check a field on a Salesforce Task';
+  /* tslint:disable-next-line:max-line-length */
+  protected stepExpression: string = 'the (?<field>[a-zA-Z0-9_]+) field on salesforce task from (?<email>.+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain) ?(?<expectedValue>.+)?';
+  protected stepType: StepDefinition.Type = StepDefinition.Type.VALIDATION;
+  protected expectedFields: Field[] = [{
+    field: 'email',
+    type: FieldDefinition.Type.EMAIL,
+    description: "Owner's email address",
+  }, {
+    field: 'field',
+    type: FieldDefinition.Type.STRING,
+    description: 'Field name to check',
+  }, {
+    field: 'operator',
+    type: FieldDefinition.Type.STRING,
+    optionality: FieldDefinition.Optionality.OPTIONAL,
+    description: 'Check Logic (be, not be, contain, not contain, be greater than, be less than, be set, not be set, be one of, or not be one of)',
+  }, {
+    field: 'expectedValue',
+    type: FieldDefinition.Type.ANYSCALAR,
+    optionality: FieldDefinition.Optionality.OPTIONAL,
+    description: 'Expected field value',
+  }];
+  protected expectedRecords: ExpectedRecord[] = [{
+    id: 'lead',
+    type: RecordDefinition.Type.KEYVALUE,
+    fields: [{
+      field: 'Id',
+      type: FieldDefinition.Type.NUMERIC,
+      description: "Lead's SalesForce ID",
+    }, {
+      field: 'CreatedDate',
+      type: FieldDefinition.Type.DATETIME,
+      description: "Lead's Created Date",
+    }, {
+      field: 'LastModifiedDate',
+      type: FieldDefinition.Type.DATETIME,
+      description: "Lead's Last Modified Date",
+    }],
+    dynamicFields: true,
+  }];
+
+  async executeStep(step: Step): Promise<RunStepResponse> {
+    const stepData: any = step.getData().toJavaScript();
+    const email: string = stepData.email;
+    const field: string = stepData.field;
+    const operator: string = stepData.operator || 'be';
+    const expectedValue: string = stepData.expectedValue;
+    const isSetOperator = ['be set', 'not be set'].includes(operator);
+    let lead: Record<string, any>;
+    let tasks: Record<string, any>[];
+
+    if (isNullOrUndefined(expectedValue) && !(operator == 'be set' || operator == 'not be set')) {
+      return this.error("The operator '%s' requires an expected value. Please provide one.", [operator]);
+    }
+
+    try {
+      lead = await this.client.findObjectByField('Lead', 'Email', email);
+    } catch (e) {
+      return this.error('There was a problem checking the Lead: %s', [e.toString()]);
+    }
+
+    try {
+      if (!lead) {
+        // If no results were found, return an error.
+        return this.fail('No Lead found with email %s', [email]);
+      }
+
+      const taskFieldMap = {
+        'WhoId': lead.Id,
+      };
+
+      console.log(taskFieldMap);
+
+      tasks = await this.client.findObjectsbyFields('Task', taskFieldMap);
+
+      const record = this.createRecord(tasks[0]);
+      const validResults = [];
+      let result = {
+        valid: false,
+        message: '',
+      };
+
+      // Assert field
+      tasks.forEach((task) => {
+        const assertResult = this.assert(operator, task[field], expectedValue, field);
+        if (assertResult.valid) {
+          validResults.push(task);
+          result = assertResult;
+        }
+      });
+
+      if (validResults.length > 1) {
+        // If no results were found, return an error.
+        return this.fail('There are more than 1 tasks from %s that matches the expected %s from email %s.', [email, field]);
+      } else if (validResults.length < 1) {
+        // If no results were found, return an error.
+        return this.fail('No tasks from %s have been found that matches the expected %s.', [email, field]);
+      }
+
+      return result.valid ? this.pass(result.message, [], [record])
+        : this.fail(result.message, [], [record]);
+
+    } catch (e) {
+      if (e instanceof util.UnknownOperatorError) {
+        return this.error('%s Please provide one of: %s', [e.message, baseOperators.join(', ')]);
+      }
+      if (e instanceof util.InvalidOperandError) {
+        return this.error(e.message);
+      }
+      return this.error('There was an error during validation of task field: %s', [e.message]);
+    }
+  }
+
+  createRecord(task: Record<string, any>) {
+    delete task.attributes;
+    return this.keyValue('task', 'Checked Task', task);
+  }
+}
+
+export { TaskFieldEquals as Step };
---
+++ test/steps/task/task-field-equals.ts
@@ -0,0 +1,219 @@
+import { Struct } from 'google-protobuf/google/protobuf/struct_pb';
+import * as chai from 'chai';
+import { default as sinon } from 'ts-sinon';
+import * as sinonChai from 'sinon-chai';
+import 'mocha';
+
+// tslint:disable-next-line:max-line-length
+import { Step as ProtoStep, StepDefinition, FieldDefinition, RunStepResponse } from '../../../src/proto/cog_pb';
+import { Step } from '../../../src/steps/task/task-field-equals';
+
+chai.use(sinonChai);
+
+describe('TaskFieldEqualsStep', () => {
+  const expect = chai.expect;
+  let protoStep: ProtoStep;
+  let stepUnderTest: Step;
+  const clientWrapperStub: any = {};
+
+  beforeEach(() => {
+    clientWrapperStub.findObjectByField = sinon.stub();
+    clientWrapperStub.findObjectsbyFields = sinon.stub();
+    stepUnderTest = new Step(clientWrapperStub);
+    protoStep = new ProtoStep();
+  });
+
+  it('should return expected step metadata', () => {
+    const stepDef: StepDefinition = stepUnderTest.getDefinition();
+    expect(stepDef.getStepId()).to.equal('TaskFieldEquals');
+    expect(stepDef.getName()).to.equal('Check a field on a Salesforce Task');
+    expect(stepDef.getExpression()).to.equal('the (?<field>[a-zA-Z0-9_]+) field on salesforce task from (?<email>.+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain) ?(?<expectedValue>.+)?');
+    expect(stepDef.getType()).to.equal(StepDefinition.Type.VALIDATION);
+  });
+
+  it('should return expected step fields', () => {
+    const stepDef: StepDefinition = stepUnderTest.getDefinition();
+    const fields: any[] = stepDef.getExpectedFieldsList().map((field: FieldDefinition) => {
+      return field.toObject();
+    });
+
+    // Field field
+    const field: any = fields.filter(f => f.key === 'field')[0];
+    expect(field.optionality).to.equal(FieldDefinition.Optionality.REQUIRED);
+    expect(field.type).to.equal(FieldDefinition.Type.STRING);
+
+    // Email field
+    const email: any = fields.filter(f => f.key === 'email')[0];
+    expect(email.optionality).to.equal(FieldDefinition.Optionality.REQUIRED);
+    expect(email.type).to.equal(FieldDefinition.Type.EMAIL);
+
+    // Operator field
+    const operator: any = fields.filter(f => f.key === 'operator')[0];
+    expect(operator.optionality).to.equal(FieldDefinition.Optionality.OPTIONAL);
+    expect(operator.type).to.equal(FieldDefinition.Type.STRING);
+
+    // Expected Value field
+    const expectedValue: any = fields.filter(f => f.key === 'expectedValue')[0];
+    expect(expectedValue.optionality).to.equal(FieldDefinition.Optionality.OPTIONAL);
+    expect(expectedValue.type).to.equal(FieldDefinition.Type.ANYSCALAR);
+  });
+
+  it('should respond with pass if API client resolves expected data', async () => {
+    // Stub a response that matches expectations.
+    const expectedUser: any = { Id: 1, someField: 'Expected Value' };
+    const expectedTasks: any = [{ someField: 'Expected Value' }];
+    const taskFieldMap = {
+      'WhoId': expectedUser.Id,
+    };
+    clientWrapperStub.findObjectByField.resolves(expectedUser);
+    clientWrapperStub.findObjectsbyFields.resolves(expectedTasks);
+
+    // Set step data corresponding to expectations
+    const expectations: any = {
+      field: 'someField',
+      expectedValue: expectedTasks[0].someField,
+      email: 'anything@example.com',
+    };
+    protoStep.setData(Struct.fromJavaScript(expectations));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(clientWrapperStub.findObjectByField).to.have.been.calledWith('Lead', 'Email', expectations.email);
+    expect(clientWrapperStub.findObjectsbyFields).to.have.been.calledWith('Task', taskFieldMap);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.PASSED);
+  });
+
+  it('should respond with error when invalid operator was passed', async () => {
+    // Stub a response that matches expectations.
+    const expectedUser: any = { someField: 'Expected Value' };
+    clientWrapperStub.findObjectByField.resolves(expectedUser);
+
+    // Set step data corresponding to expectations
+    const expectations: any = {
+      field: 'someField',
+      expectedValue: expectedUser.someField,
+      email: 'anything@example.com',
+      operator: 'invalid operator',
+    };
+    protoStep.setData(Struct.fromJavaScript(expectations));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with error when actual and expected values have different types and compared', async () => {
+    // Stub a response that matches expectations.
+    const expectedUser: any = { someField: 'Expected Value', numericField: 5000 };
+    clientWrapperStub.findObjectByField.resolves(expectedUser);
+
+    // Set step data corresponding to expectations
+    const expectations: any = {
+      field: 'numericField',
+      expectedValue: 'nonNumeric',
+      email: 'anything@example.com',
+      operator: 'be greater than',
+    };
+    protoStep.setData(Struct.fromJavaScript(expectations));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with fail if API client resolves unexpected data', async () => {
+    // Stub a response that does not match expectations.
+    const expectedUser: any = { someField: 'Expected Value' };
+    const expectedTasks: any = [{ someField: expectedUser.someField }];
+
+    clientWrapperStub.findObjectByField.resolves(expectedUser);
+    clientWrapperStub.findObjectsbyFields.resolves(expectedTasks);
+
+    // Set step data corresponding to expectations
+    protoStep.setData(Struct.fromJavaScript({
+      field: 'someField',
+      expectedValue: `Not ${expectedUser.someField}`,
+      email: 'anything@example.com',
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.FAILED);
+  });
+
+  it('should respond with fail if API client resolves no results', async () => {
+    // Stub a response with no results in the body.
+    clientWrapperStub.findObjectByField.resolves(null);
+
+    protoStep.setData(Struct.fromJavaScript({
+      field: 'anyField',
+      expectedValue: 'Any Value',
+      email: 'anything@example.com',
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.FAILED);
+  });
+
+  it('should respond with error if API client returns error', async () => {
+    // Stub a response that responds with error.
+    const error: Error = new Error('Any error');
+    clientWrapperStub.findObjectByField.throws(error);
+
+    protoStep.setData(Struct.fromJavaScript({
+      field: 'someOtherField',
+      expectedValue: 'Any Value',
+      email: 'anything@example.com',
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it('should respond with error when inputing invalid operator', async () => {
+    // Stub a response that responds with error.
+    const expectedUser: any = { someField: 'Expected Value' };
+    clientWrapperStub.findObjectByField.resolves(expectedUser);
+
+    protoStep.setData(Struct.fromJavaScript({
+      field: 'someField',
+      expectedValue: 'Any Value',
+      operator: 'invalidOperator',
+      email: 'anything@example.com',
+    }));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it("should respond with error when expectedValue is null when operator is not 'be set' or 'not be set'", async () => {
+    // Stub a response that matches expectations.
+    const expectedResponseMessage: string = "The operator '%s' requires an expected value. Please provide one.";
+
+    // Set step data corresponding to expectations
+    const expectations: any = {
+      field: 'anyField',
+      expectedValue: null,
+      email: 'anyEmail',
+      operator: 'anyOperator',
+    };
+    protoStep.setData(Struct.fromJavaScript(expectations));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getMessageFormat()).to.equal(expectedResponseMessage);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+
+  it("should respond with error when expectedValue is not provided when operator is not 'be set' or 'not be set'", async () => {
+    // Stub a response that matches expectations.
+    const expectedResponseMessage: string = "The operator '%s' requires an expected value. Please provide one.";
+
+    // Set step data corresponding to expectations
+    const expectations: any = {
+      field: 'anyField',
+      email: 'anyEmail',
+      operator: 'anyOperator',
+    };
+    protoStep.setData(Struct.fromJavaScript(expectations));
+
+    const response: RunStepResponse = await stepUnderTest.executeStep(protoStep);
+    expect(response.getMessageFormat()).to.equal(expectedResponseMessage);
+    expect(response.getOutcome()).to.equal(RunStepResponse.Outcome.ERROR);
+  });
+});
