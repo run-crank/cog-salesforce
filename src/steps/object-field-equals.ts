f9d0e9725d2d2e38d68af7f9638cd8ada41b4427
Russell Hart <russell.hart@stackmoxie.com>
2023-02-08 11:00:31 -0500

    Change targetObject to Any Object

--- src/steps/object-field-equals.ts
+++ src/steps/object-field-equals.ts
@@ -14,7 +14,7 @@ export class ObjectFieldEquals extends BaseStep implements StepInterface {
   protected stepExpression: string = 'the (?<field>[a-zA-Z0-9_]+) field on salesforce (?<objName>[a-zA-Z0-9]+) object with id (?<id>[^\s]+) should (?<operator>be set|not be set|be less than|be greater than|be one of|be|contain|not be one of|not be|not contain|match|not match) ?(?<expectedValue>.+)?';
   protected stepType: StepDefinition.Type = StepDefinition.Type.VALIDATION;
   protected actionList: string[] = ['check'];
-  protected targetObject: string = 'Object';
+  protected targetObject: string = 'Any Object';
   protected expectedFields: Field[] = [{
     field: 'objName',
     type: FieldDefinition.Type.STRING,
